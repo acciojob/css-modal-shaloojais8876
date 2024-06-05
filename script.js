bb052eb769e50d31fea8156c734adc7b480fc2e5
shaloojais8876 <104994730+shaloojais8876@users.noreply.github.com>
2024-06-05 19:01:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', (event) => {
+  const modal = document.getElementById('myModal');
+  const openModalButton = document.getElementById('openModal');
+  const closeModalButton = document.querySelector('.close-modal');
+
+  openModalButton.addEventListener('click', () => {
+    modal.style.display = 'block';
+  });
+
+  closeModalButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+
+  window.addEventListener('click', (event) => {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  });
+});
